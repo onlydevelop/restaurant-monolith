8737d1347e63209b4288e6fbe3d06fdbc1fcf751
Dipanjan Bhowmik <onlydevelop@gmail.com>
2018-07-03 14:26:07 +0530

    Added Repository and removed hardcoding.

--- src/main/java/com/restaurant/restaurantmonolith/ItemsController.java
+++ src/main/java/com/restaurant/restaurantmonolith/ItemsController.java
@@ -1,7 +1,9 @@
 package com.restaurant.restaurantmonolith;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ItemsController {
 	
+	@Autowired
+	private ItemRepository repository;
+	
 	@GetMapping("/items/{id}")
-	public Item getItem(@PathVariable String name) {
-		return new Item(1001, name, BigDecimal.valueOf(220)); 
+	public Item getItem(@PathVariable Long id) {
+		
+		Optional<Item> item = repository.findById(id);
+		return item.get(); 
 	}
 }
