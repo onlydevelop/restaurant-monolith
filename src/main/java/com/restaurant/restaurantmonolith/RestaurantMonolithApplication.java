42b4e24fc52d784225e795f9ababb235df4fe024
Dipanjan Bhowmik <onlydevelop@gmail.com>
2018-07-03 12:48:52 +0530

    Initial commit

---
+++ src/main/java/com/restaurant/restaurantmonolith/RestaurantMonolithApplication.java
@@ -0,0 +1,12 @@
+package com.restaurant.restaurantmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantMonolithApplication.class, args);
+	}
+}
